11dd67e43f028669db25d36a1f351973280d036e
Maruf <66161467+marufpbt@users.noreply.github.com>
2021-01-30 00:18:50 +0600

    good phase

--- style.js
+++ style.js
@@ -1,21 +1,4 @@
 
-document.getElementById("firstClassIncrease").addEventListener("click", function () {
-	handleTicketChange('firstClass', true);
-})
-
-
-document.getElementById("firstClassDecrease").addEventListener("click", function () {
-	handleTicketChange('firstClass', false);
-})
-
-document.getElementById("economyIncrease").addEventListener("click", function () {
-	handleTicketChange('economy', true);
-})
-
-document.getElementById("economyDecrease").addEventListener("click", function () {
-	handleTicketChange('economy', false);
-})
-
 function handleTicketChange(ticket, isIncrease) {
 	const ticketInput = document.getElementById(ticket + 'Input');
 	const ticketCount = parseInt(ticketInput.value);
@@ -27,28 +10,19 @@ function handleTicketChange(ticket, isIncrease) {
 		ticketNewCount = ticketCount - 1;
 	}
 	ticketInput.value = ticketNewCount;
-	ticketTotal = 0;
-	if (ticket == 'economy') {
-		ticketTotal = ticketNewCount * 1229;
-	}
-	if (ticket == 'firstClass') {
-		ticketTotal = ticketNewCount * 59;
-	}
-	document.getElementById(ticket + 'Total').innerText = ticketTotal;
 
 	calculateTotal();
-
 }
 
 
-function calculateTotal(){
+function calculateTotal() {
 	const firstClassInput = document.getElementById("firstClassInput");
 	const firstClassCount = parseInt(firstClassInput.value);
 
 	const economyInput = document.getElementById("economyInput");
 	const economyCount = parseInt(economyInput.value);
 
-	const subTotal = firstClassCount * 59 + economyCount * 1229;
+	const subTotal = firstClassCount * 150 + economyCount * 100;
 	document.getElementById("subTotal").innerText = subTotal;
 
 	const tax = Math.round(subTotal * .1);
